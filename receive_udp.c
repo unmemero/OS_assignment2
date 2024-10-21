cf94cc5897a0d65ca106d6ee658e833d7103da15
Rafael Garcia <fafagm99@gmail.com>
2024-10-20 20:40:12 -0600

    Tested connection in dandelion

--- receive_udp.c
+++ receive_udp.c
@@ -5,59 +5,119 @@
 #include <errno.h>
 #include <stdint.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFFSIZE 481 //Not sure about this value, may need to do it client side
 
 typedef long long int muy_largo_t; // Easier to write
 
-/*Returns port value in pointer, and status code as regulae*/
-static int convert_port_name(uint16_t *port, const char *port_name){
-	/*Port name doesn't exist, or null char*/
-	if(port_name == NULL || *port_name == '\0')return -1;
+/*Better write*/
+ssize_t better_write(int fd, const char *buf, size_t count) {
+    size_t already_written = 0;
+    size_t to_be_written = count;
+    ssize_t res_write;
 
-	/*Converts string to long long int*/
-	char *end;
-	muy_largo_t nn = strtoll(port_name, &end, 0);
-	if(*end != '\0')return -1;
+    if (count == 0) return count;
 
-	/*Checks if port is in range*/
-	if(nn < (muy_largo_t)0)return -1;
-
-	muy_largo_t t = (uint16_t)nn;
-	muy_largo_t tt = (muy_largo_t)t;
-
-	/*Checks if port is in range*/
-	if(tt != nn)return -1;
-
-	/*Returns port value*/
-	*port = (uint16_t)nn;
-
-	/*All clear*/
-	return 0;
+    while (to_be_written > 0) {
+        res_write = write(fd, &buf[already_written], to_be_written);
+        if (res_write < 0) {
+            return res_write; // Error
+        }
+        if (res_write == 0) {
+            return already_written; // Nothing written, stop
+        }
+        already_written += res_write;
+        to_be_written -= res_write;
+    }
+    return already_written;
 }
 
+/*Returns port value in pointer, and status code as regular*/
+static int convert_port_name(uint16_t *port, const char *port_name) {
+    if (port_name == NULL || *port_name == '\0') return -1;
 
-int main(int argc, char *argv[]){
-	/*Check if we have port as arg*/
-	if(argc < 2){
-		fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-		return 1;
-	}
-	
-	/*Get port num*/
-	uint16_t port;
-	if(convert_port_name(&port, argv[1]) < 0){
-		fprintf(stderr, "Invalid port number: %s\n", argv[1]);
-		return 1;
-	}
+    char *end;
+    muy_largo_t nn = strtoll(port_name, &end, 0);
+    if (*end != '\0') return -1;
+    if (nn < 0) return -1;
 
-	/*Create socket*/
-	int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    uint16_t t = (uint16_t)nn;
+    muy_largo_t tt = (muy_largo_t)t;
+    if (tt != nn) return -1;
 
-	/*Socket failed*/
-	if(sockfd < 0){
-		fprintf(stderr, "socket() failed: %s\n", strerror(errno));
-		return 1;
-	}
+    *port = t;
+    return 0;
+}
 
-	return 0;
+int main(int argc, char *argv[]) {
+    /*Check if we have port as arg*/
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    /*Get port num*/
+    uint16_t port;
+    if (convert_port_name(&port, argv[1]) < 0) {
+        fprintf(stderr, "Invalid port number: %s\n", argv[1]);
+        return 1;
+    }
+
+    /*Create socket*/
+    int socket_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+    /*Socket failed*/
+    if (socket_fd < 0) {
+        fprintf(stderr, "Failed creating socket: %s\n", strerror(errno));
+        return 1;
+    }
+
+    /*Bind socket*/
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port); /* Convert port to network byte order */
+    server_address.sin_addr.s_addr = INADDR_ANY; /* Bind to all interfaces */
+
+    /*Binding*/
+    int bind_status = bind(socket_fd, (struct sockaddr *)&server_address, sizeof(server_address));
+    /*Bind failed*/
+    if (bind_status < 0) {
+        fprintf(stderr, "Failed binding socket: %s\n", strerror(errno));
+        close(socket_fd);
+        return 1;
+    }
+
+    /*Receive data*/
+    char buffer[BUFFSIZE];
+    int bytes_received;
+
+    /*Receive and print data*/
+    while ((bytes_received = recv(socket_fd, buffer, BUFFSIZE, 0)) > 0) {
+        buffer[bytes_received] = '\0'; /* Null-terminate the received data */
+        if (better_write(STDOUT_FILENO, buffer, bytes_received) < 0) {
+            fprintf(stderr, "Failed writing data: %s\n", strerror(errno));
+            close(socket_fd);
+            return 1;
+        }
+    }
+
+    /*FAILED RECV*/
+    if (bytes_received < 0) {
+        fprintf(stderr, "Failed receiving data: %s\n", strerror(errno));
+        close(socket_fd);
+        return 1;
+    }
+
+    /*Close socket*/
+    if (close(socket_fd) < 0) {
+        fprintf(stderr, "Failed closing socket: %s\n", strerror(errno));
+        return 1;
+    }
+
+    /*All clear*/
+    return 0;
 }
